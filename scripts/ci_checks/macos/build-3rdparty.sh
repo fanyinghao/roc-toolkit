bd1a4f04cacce075cfc71e8ccd4a8e99c12a9c0b
Yinghao Fan <yinghao_fan@legend.tech>
2025-11-16 21:53:42 +0800

    ci: Update macOS build workflow to use build-3rdparty script
    Modify the macOS build workflow to replace the standard build script with the build-3rdparty script for both macOS 14 and 15. Additionally, enhance the build-3rdparty script to include debug build options, enabling tests, benchmarks, examples, and sanitizers.

--- scripts/ci_checks/macos/build-3rdparty.sh
+++ scripts/ci_checks/macos/build-3rdparty.sh
@@ -5,6 +5,19 @@ set -euxo pipefail
 brew install --quiet --force --overwrite \
      automake scons
 
+# debug build
+scons -Q \
+      --enable-werror \
+      --enable-tests \
+      --enable-benchmarks \
+      --enable-examples \
+      --enable-debug \
+      --sanitizers=all \
+      --enable-static \
+      --build-3rdparty=all \
+      test
+
+# release build
 scons -Q \
       --enable-werror \
       --enable-tests \
